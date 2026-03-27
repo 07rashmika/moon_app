bfec65148d7af72b065435d8737265b001a74866
07rashmika <rashmikadesilva07@gmail.com>
2026-03-28 01:08:13 +0530

    -corrected the env and gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:moon_app/features/auth/presentation/screens/auth_screen.dart';
 import 'package:moon_app/firebase_options.dart';
 import 'package:moon_app/themes/dark_mode.dart';
@@ -8,6 +9,7 @@ import 'package:moon_app/themes/light_mode.dart';
 void main() async {
   //firebase setup
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(const MyApp());
